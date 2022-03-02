4bb5e75c892376c93835672df207e9fb7097d614
helloecho12345 <92740628+helloecho12345@users.noreply.github.com>
2022-03-02 14:43:36 +0000

    game opens window with red rectangle that can be moved within boundary of window

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import pygame
-
 pygame.init()
 
-WIDTH, HEIGHT = 900, 500   #in caps as they are constants
+win = pygame.display.set_mode((500, 500))
 
-from pygame.locals import (
-  RLEACCEL, # does something to allow uploaded images to render on the screen
-)
+pygame.display.set_caption("First Game")
 
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+x=50
+y=425
+width=40
+height=60
+vel=5
 
+run = True
+while run:
+  pygame.time.delay(100)
+  #check for event
+  for event in pygame.event.get():
+    #loops through events
+    if event.type == pygame.QUIT:
+      #if you hit big red button in corner to close window, then 
+      run=False
 
-background_surface = pygame.image.load("hacker.jpeg").convert()
-background_surface = pygame.transform.scale(background_surface, (WIDTH, HEIGHT))
-
-
-#define the main function for the game
-def main():
-
-  run = True
+  keys = pygame.key.get_pressed()
   
-  while run:
+  if keys[pygame.K_LEFT] and x > vel:
+    x -= vel
+    #vel changes speed of movement
+  if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+    #character not allowed to move off right of screen
+    #width is the width of the character
+    #the position of the character will not be allowed to move past the border now set the width of the character from the edge
+    x += vel
+  if keys[pygame.K_UP] and y > vel:
+    y -= vel
+  if keys[pygame.K_DOWN] and y < 500 - height - vel:
+    y += vel
+  
+  win.fill((0, 0, 0))
+  #this will fill the background with black so you don't see a trail of red rectangles
 
-    for event in pygame.event.get():
-      #this gets us all the events, loops through them and we can check what they were
-      if event.type == pygame.QUIT:
-        run = False
+  pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+  pygame.display.update()
 
-    WINDOW.blit(background_surface, (0,0))
-
-    #pygame.quit()
-    pygame.display.flip()
-
-if __name__ == "__main__":
-  main() 
+pygame.quit()
