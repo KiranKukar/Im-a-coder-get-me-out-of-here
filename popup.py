5272e5b70a3db48193f8714a3a181a4c807a0aa1
helloecho12345 <92740628+helloecho12345@users.noreply.github.com>
2022-03-07 17:02:29 +0000

    created Button class and Textbox class

--- popup.py
+++ popup.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_gui
 
+
 class Popup():
     def __init__(self, win_width, win_height):
       self.manager = pygame_gui.UIManager((win_width, win_height)) 
@@ -23,41 +24,45 @@ class Popup():
       answer_button_width = 375
       answer_button_height = 50
 
-      answer_button_rect_1 = pygame.Rect(answer_button_padding_left, 230, answer_button_width, answer_button_height)
-      answer_button_rect_2 = pygame.Rect(answer_button_padding_left, 280, answer_button_width, answer_button_height)
-      answer_button_rect_3 = pygame.Rect(answer_button_padding_left, 330, answer_button_width, answer_button_height)
-      answer_button_rect_4 = pygame.Rect(answer_button_padding_left, 380, answer_button_width, answer_button_height)
-      question_rect = pygame.Rect(50, 50, 375, 300)
+      answers = [
+        "Answer 1 goes here.",
+        "Answer 2 goes here.",
+        "Answer 3 goes here.",
+        "Answer 4 goes here.",
+      ]
 
-      self.answer_button_1 = pygame_gui.elements.UIButton(
-                             relative_rect=answer_button_rect_1,
-                             text='Click me to make me disappear',
-                             manager=self.manager,
-                             container=self.ui_window)
 
-      self.answer_button_2 = pygame_gui.elements.UIButton(
-                             relative_rect=answer_button_rect_2,
-                             text='Click me to make me disappear',
-                             manager=self.manager,
-                             container=self.ui_window)
 
-      self.answer_button_3 = pygame_gui.elements.UIButton(
-                             relative_rect=answer_button_rect_3,
-                             text='Click me to make me disappear',
-                             manager=self.manager,
-                             container=self.ui_window)
+      answer_button_1 = Button(answer_button_padding_left, 205, answer_button_width, answer_button_height, answers[0], self.manager, self.ui_window)
+      answer_button_2 = Button(answer_button_padding_left, 255, answer_button_width, answer_button_height, answers[1], self.manager, self.ui_window)
+      answer_button_3 = Button(answer_button_padding_left, 305, answer_button_width, answer_button_height, answers[2], self.manager, self.ui_window)
+      answer_button_4 = Button(answer_button_padding_left, 355, answer_button_width, answer_button_height, answers[3], self.manager, self.ui_window)
 
-      self.answer_button_4 = pygame_gui.elements.UIButton(
-                             relative_rect=answer_button_rect_4,
-                             text='Click me to make me disappear',
-                             manager=self.manager,
-                             container=self.ui_window)    
+      question_text = "Hello there, can you tell me, what is the best way to why are you going to how come the first time a which way up is the what?"
+      question_textbox = Textbox(50, 40, 375, 150, question_text, self.manager, self.ui_window)
+      question_textbox.text_effect_typing_appear()
 
-      self.question_label = pygame_gui.elements.UILabel(
-                             relative_rect=question_rect,
-                             text='Question text, what is the time?',
-                             manager=self.manager,
-                             container=self.ui_window)      
+class Button():
+  def __init__(self, left_padding, top_padding, width, height, text, manager, container):
+    self.element = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect(left_padding, top_padding, width, height),
+                        text=f'{text}',
+                        manager=manager,
+                        container=container)
 
-    
-      self.question_label.set_active_effect(pygame_gui.TEXT_EFFECT_TYPING_APPEAR, params = {'time_per_letter': .05})
+class Textbox():
+  def __init__(self, left_padding, top_padding, width, height, html_text, manager, container):
+    self.element = pygame_gui.elements.UITextBox(
+                        relative_rect=pygame.Rect(left_padding, top_padding, width, height),
+                        html_text=f'{html_text}',
+                        manager=manager,
+                        container=container)        
+
+  def text_effect_typing_appear(self):
+    self.element.set_active_effect(pygame_gui.TEXT_EFFECT_TYPING_APPEAR, params = {'time_per_letter': .01})
+
+
+                      
+          
+
+
