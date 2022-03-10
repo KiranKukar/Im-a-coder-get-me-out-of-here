613f21329cd500490d169d9a5ef9f28e09e46de3
ElliottZaki <spudnick99@hotmail.com>
2022-03-10 11:42:05 +0000

    Refractored Menu, linked to main and moved to seperate py file

--- menu.py
+++ menu.py
@@ -33,95 +33,95 @@ clock = pygame.time.Clock()
 
 class Menu():
     def __init__(self):
+        self.main_menu()
 
-        def draw_header(text, font, colour, surface, x, y):
-            textobj = font.render(text, 1, colour) 
-            textrect = textobj.get_rect()
-            textrect.topleft = (x, y)
-            # textrect.center = (x // 2, y // 2)
-            surface.blit(textobj, textrect)
+    def draw_header(self, text, font, colour, surface, x, y):
+        textobj = font.render(text, 1, colour) 
+        textrect = textobj.get_rect()
+        textrect.topleft = (x, y)
+        # textrect.center = (x // 2, y // 2)
+        surface.blit(textobj, textrect)
 
-        # def draw_text(text, smallText, colour, surface, x, y):
-        #     textobj = smallText.render(text, 1, colour) 
-        #     textrect = textobj.get_rect()
-        #     textrect.center = (x // 2, y // 2)
-        #     surface.blit(textobj, textrect)
+    # def draw_text(text, smallText, colour, surface, x, y):
+    #     textobj = smallText.render(text, 1, colour) 
+    #     textrect = textobj.get_rect()
+    #     textrect.center = (x // 2, y // 2)
+    #     surface.blit(textobj, textrect)
 
 
-        def main_menu():
-            while True:
-                # import main
-                win.fill((0,0,0))
-                draw_header('main menu', font, (255, 255, 255), win, 20, 20)
+    def main_menu(self):
+        while True:
+            # import main
+            win.fill((0,0,0))
+            self.draw_header('main menu', font, (255, 255, 255), win, 20, 20)
 
-                if start_button.draw(win):
-                    print('Start')
-                    first_screen()
-                if exit_button.draw(win):
+            if start_button.draw(win):
+                print('Start')
+                self.first_screen()
+            if exit_button.draw(win):
+                pygame.quit()
+                sys.exit()
+            if about_button.draw(win):
+                print('About')
+            if credits_button.draw(win):
+                print ('Credits')
+    
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if about_button.draw(win):
-                    print('About')
-                if credits_button.draw(win):
-                    print ('Credits')
-        
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            pygame.quit()
-                            sys.exit()
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            click = True
-        
-                pygame.display.update()
-                # clock.tick(60)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
+    
+            pygame.display.update()
+            # clock.tick(60)
 
-        def first_screen():
-            while True:
-                
-                win.fill((0,0,0))
-                draw_header('Briefing:', font, (255, 255, 255), win, 20, 20)
-                # draw_text(story_text, smallText, (255, 255, 255), win, 20, 600)
+    def first_screen(self):
+        while True:
+            import main
+            win.fill((0,0,0))
+            self.draw_header('instructions:', font, (255, 255, 255), win, 20, 20)
+            # draw_text(story_text, smallText, (255, 255, 255), win, 20, 600)
 
-                mx, my = pygame.mouse.get_pos()
+            mx, my = pygame.mouse.get_pos()
 
-                if start_button.draw(win):
-                    import main
-                    print('Start')
-                    main.game()
-                if exit_button.draw(win):
-                    main_menu()
-        
-                # button_1 = pygame.Rect(50, 100, 200, 50)
-                # textSurf, textRect = button_1("Start game!", smallText)
-                # textRect.center = ( (150+(100/2)), (450+(50/2)) )
-                # gameDisplay.blit(textSurf, textRect)
+            if start_button.draw(win):
+                print('Start')
+                main.game()
+            if exit_button.draw(win):
+                self.main_menu()
+    
+            # button_1 = pygame.Rect(50, 100, 200, 50)
+            # textSurf, textRect = button_1("Start game!", smallText)
+            # textRect.center = ( (150+(100/2)), (450+(50/2)) )
+            # gameDisplay.blit(textSurf, textRect)
 
-                # button_2 = pygame.Rect(50, 200, 200, 50)
-                # #This is to select the box with your mouse.
-                # if button_1.collidepoint((mx, my)):
-                #     if click:
-                #         game()
-                # if button_2.collidepoint((mx, my)):
-                #     if click:
-                #         main_menu()
-                # pygame.draw.rect(win, (255, 0, 0), button_1)
-                # pygame.draw.rect(win, (255, 0, 0), button_2)
+            # button_2 = pygame.Rect(50, 200, 200, 50)
+            # #This is to select the box with your mouse.
+            # if button_1.collidepoint((mx, my)):
+            #     if click:
+            #         game()
+            # if button_2.collidepoint((mx, my)):
+            #     if click:
+            #         main_menu()
+            # pygame.draw.rect(win, (255, 0, 0), button_1)
+            # pygame.draw.rect(win, (255, 0, 0), button_2)
 
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            running = False
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            click = True
-                
-                pygame.display.update()
-                clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
+            
+            pygame.display.update()
+            clock.tick(60)
