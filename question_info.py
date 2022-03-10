55330f357f3ac5e47aaf906cd0d98357fec7726f
Alexander Jamshidi <alexanderjamshidi@Alexanders-Air.localdomain>
2022-03-10 19:27:55 +0000

    questions added

--- question_info.py
+++ question_info.py
@@ -23,54 +23,54 @@ class Question_setup():
     self.question_bank = question_bank
 
     q1 = {
-      'question' : 'Which number is largest?',
-      'correct_answer' : '4',
-      'incorrect_answers' : ['1', '2', '3']
+      'question' : "Using Python, how would you print the string: 'Hello World'?",
+      'correct_answer' : "print('Hello World')",
+      'incorrect_answers' : ["print(Hello world)","print{'Hello World'}","print'{Hello World}'"]
     }
     q2 = {
-      'question' : 'Which letter is a vowel?',
-      'correct_answer' : 'A',
-      'incorrect_answers' : ['B', 'C', 'D']
+      'question' : 'Using Python, what does // mean?',
+      'correct_answer' : 'Floor division',
+      'incorrect_answers' : ['Next line', 'Railroad Ahead', 'Divide twice']
     }
     q3 = {
-      'question' : 'Which number is largest?',
-      'correct_answer' : '40',
-      'incorrect_answers' : ['10', '20', '30']
+      'question' : 'Using Python, what does the following code output:<br><br>fingers = 5<br># print(fingers)',
+      'correct_answer' : 'Nothing',
+      'incorrect_answers' : ['An error', '5', 'hand']
     }
     q4 = {
-      'question' : 'Which number is smallest?',
-      'correct_answer' : '-4',
-      'incorrect_answers' : ['-1', '-2', '-3']
+      'question' : 'Why is correct indentation so fundamental in Python?',
+      'correct_answer' : 'defines a block of code',
+      'incorrect_answers' : ['keeps code readable', 'can be used instead of a line break', 'all good code should look like a staircase']
     }
     q5 = {
-      'question' : 'Which letter is a vowel?',
-      'correct_answer' : 'E',
-      'incorrect_answers' : ['F', 'G', 'H']
+      'question' : "Using Python, what does the following code output:<br><br>names = ['tam', 'tim']<br>name = 'tom'<br>print(names.append(name))",
+      'correct_answer' : 'none',
+      'incorrect_answers' : ['tom', "['tam', 'tim', 'tom']", "['tom', 'tam', 'tim']"]
     }
     q6 = {
-      'question' : 'Which number is largest?',
-      'correct_answer' : '1000',
-      'incorrect_answers' : ['750', '500', '250']
+      'question' : 'Ruby is to hash as Python is to?',
+      'correct_answer' : 'Dictionary',
+      'incorrect_answers' : ['Library', 'Reference_Table', 'kif']
     }
     q7 = {
-      'question' : 'Which planet is largest?',
-      'correct_answer' : 'Jupiter',
-      'incorrect_answers' : ['Saturn', 'Neptune', 'Uranus']
+      'question' : 'Using Python, what is a tuple?',
+      'correct_answer' : 'An immutable list',
+      'incorrect_answers' : ['A list limited to 2 or 3 objects', 'A mutable set', 'tuples are found in JS, not Python']
     }
     q8 = {
-      'question' : 'Which ocean is largest?',
-      'correct_answer' : 'Pacific',
-      'incorrect_answers' : ['Indian', 'Atlantic', 'Southern']
+      'question' : 'Which of the following is the correct extension of a Python file?',
+      'correct_answer' : '.py',
+      'incorrect_answers' : ['.python', '.p', '.pn']
     }
     q9 = {
-      'question' : 'Which country is largest by area?',
-      'correct_answer' : 'Russia',
-      'incorrect_answers' : ['Canada', 'China', 'Australia']
+      'question' : 'What will be the value of the following Python expression?<br><br><br>4 + 3 % 5',
+      'correct_answer' : '7',
+      'incorrect_answers' : ['2', '4', '1']
     }
     q10 = {
-      'question' : 'Which desert is largest?',
-      'correct_answer' : 'Antarctica',
-      'incorrect_answers' : ['Kalahari', 'Gobi', 'Sahara']
+      'question' : 'Which keyword  is used for function in Python language?',
+      'correct_answer' : 'Def',
+      'incorrect_answers' : ['Define', 'define', 'Function']
     }
 
     questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
